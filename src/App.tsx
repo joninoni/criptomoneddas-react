06e2b07d7270831eaaa5df3cb3dcd66ce358d3fa
joninoni <jonibeltranhayer@gmail.com>
2024-07-25 17:34:30 -0600

    agrego los primeros estilos basicos al diseño

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 function App() {
 	return (
-		<h1>Cotizador</h1>
+		<>
+			<div className="container">
+				<h1 className="app-title">
+					Cotizador de 
+					<span>Criptomonedas</span>				
+				</h1>
+
+				<div className="content">
+
+				</div>
+			</div>
+		</>
 	)
 }
 
